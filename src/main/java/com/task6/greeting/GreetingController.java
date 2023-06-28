3639761e0de0698f92bcf9f7a39b3e8626eb3856
tomoyasu <tomoyasu0504.9115@gmail.com>
2023-06-28 18:17:28 +0900

    Hello Worldの出力を現在日時、時間の出力に変更

--- src/main/java/com/task6/greeting/GreetingController.java
+++ src/main/java/com/task6/greeting/GreetingController.java
@@ -3,10 +3,12 @@ package com.task6.greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class GreetingController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    @GetMapping("/localdatetime")
+    public LocalDateTime getLocalDateTime() {
+        return LocalDateTime.now();
     }
 }
