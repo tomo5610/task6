d3685d0867dd8c252df1343ae93c9fc4cfe45e98
tomoyasu <tomoyasu0504.9115@gmail.com>
2023-06-28 17:18:20 +0900

    Hello Worldを出力

---
+++ src/main/java/com/task6/greeting/GreetingController.java
@@ -0,0 +1,12 @@
+package com.task6.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
